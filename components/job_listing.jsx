a619f85dc55bd5ba6b6dafd13a6767073508c686
Pwersa <indaya42@gmail.com>
2022-12-04 22:09:29 +0800

    ADDED ITEM LISTING

---
+++ components/job_listing.jsx
@@ -0,0 +1,40 @@
+import styles from "./job_listing.module.css";
+
+function Homepage(){
+    return(
+        <div className={styles.container}>
+
+            
+            <img src="images/profile_rectangle.png" />
+            
+            <br /><br />
+            <div className={styles.forms}>
+                <h1>LIST A KEYBOARD</h1>
+                <br /><br />
+                <form className={styles.login_form}>
+                    <label className={styles.details_label}>Item Name:</label>
+                    <br />
+                    <input className={styles.details_input}type="text" />
+                    <br /><br /><br />
+                    <label className={styles.details_label}>Details:</label>
+                    <br />
+                    <input className={styles.details_input}type="text" />
+                    <br /><br /><br />
+                    <label className={styles.details_label}>Location:</label>
+                    <br />
+                    <input className={styles.details_input}type="text" />
+                    <br /><br /><br />
+                    <label className={styles.details_label}>Price:</label>
+                    <br />
+                    <input className={styles.details_input}type="number" />
+                    <br /><br /><br />
+                    <button className={styles.button_submit} type="submit">Confirm</button>
+                    <br />
+                </form>
+                <br />
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
---
+++ pages/job_listing.js
@@ -0,0 +1,14 @@
+import Job_Listing from "../components/job_listing";
+import Navbar from "../components/navbar_listing";
+
+function Home(){
+    return(
+        <div>
+            <Navbar />
+            <Job_Listing />
+            
+        </div>
+    );
+};
+
+export default Home;
