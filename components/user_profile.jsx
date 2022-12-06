57f31c55e31d9587d33b45efbabd7aa16fdfdbc9
Pwersa <indaya42@gmail.com>
2022-12-06 22:31:01 +0800

    redesigned user profile

--- components/user_profile.jsx
+++ components/user_profile.jsx
@@ -5,7 +5,7 @@ function Homepage(){
         <div className={styles.container}>
             <div className={styles.title}>
                 <h1>USER PROFILE</h1>
-                <button>Log Out</button>
+                <button className={styles.title_button}>Log Out</button>
             </div>
 
             <img src="images/profile_rectangle.png" />
@@ -16,25 +16,25 @@ function Homepage(){
                 
                 <table>
                     <tr>
-                        <td className={styles.title_user}>Name:</td>
+                        <td className={styles.title_user}>Name</td>
                         <td className={styles.title_detail}>JR Angelo Indaya</td>
                     </tr>
                     <tr>
-                        <td className={styles.title_user}>Email:</td>
+                        <td className={styles.title_user}>Email</td>
                         <td className={styles.title_detail}>example_email@example.com</td>
                     </tr>
                     <tr>
-                        <td className={styles.title_user}>Address:</td>
+                        <td className={styles.title_user}>Address</td>
                         <td className={styles.title_detail}>Lorem ipsum dolor sit amet consectetur adipisicing elit. A soluta et velit nostrum aliquid.</td>
 
                     </tr>
                     <tr>
-                        <td className={styles.title_user}>Contact Number:</td>
+                        <td className={styles.title_user}>Contact Number</td>
                         <td className={styles.title_detail}>xxxxxxxxxxx</td>
 
                     </tr>
                     <tr>
-                        <td className={styles.title_user}>Birthday:</td>
+                        <td className={styles.title_user}>Birthday</td>
                         <td className={styles.title_detail}>xx-xx-xxx</td>
 
                     </tr>
@@ -44,20 +44,20 @@ function Homepage(){
             <div className={styles.card_more_details}>
                 <table>
                     <tr>
-                        <td className={styles.title_user_more_detail}>Date Joined: </td>
+                        <td className={styles.title_user_more_detail}>Date Joined</td>
                         <td className={styles.title_more_detail}>xx-xx-xxxx</td>
                     </tr>
                     <tr>
-                        <td className={styles.title_user_more_detail}>Ratings: </td>
+                        <td className={styles.title_user_more_detail}>Ratings</td>
                         <td className={styles.title_more_detail}>example_email@example.com</td>
                     </tr>
                     <tr>
-                        <td className={styles.title_user_more_detail}>Number of Items Bought: </td>
+                        <td className={styles.title_user_more_detail}>Number of Items Bought</td>
                         <td className={styles.title_more_detail}>100</td>
 
                     </tr>
                     <tr>
-                        <td className={styles.title_user_more_detail}>Number of Items Sold: </td>
+                        <td className={styles.title_user_more_detail}>Number of Items Sold</td>
                         <td className={styles.title_more_detail}>xxxxxxxxxxx</td>
 
                     </tr>
