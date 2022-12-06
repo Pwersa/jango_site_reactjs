72d80a042f3246db52b10104639014356297e9e5
Pwersa <indaya42@gmail.com>
2022-12-06 21:33:48 +0800

    redesign homepage

--- components/navbar_homepage.jsx
+++ components/navbar_homepage.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
       <div className={styles.container}>
         <div className={styles.title}>
           <img src="images/icon_badge.png" width="90" height="90" />  
-          <h1>JANGO KEEB</h1>
+          <h1>Jango Keebs</h1>
         </div>
         
         <div className={styles.navbarButtons}>
-          <a href="#">About Us</a>
-          <a href="#">Log In</a>
-          <button>Sign Up</button>
+          <a className={styles.about_us} href="http://localhost:3000/about_us">About Us</a>
+          <a className={styles.log_in} href="#">Log In</a>
+          <button className={styles.sign_up} >Sign Up</button>
         </div>
       </div>
     );
